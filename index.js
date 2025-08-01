ab8851cf7da3d91ee7f9556ece3b7e07c1c3b05b
PoojithaSiddamsetty373 <siddamsettypoojitha@gmail.com>
2025-08-01 15:52:28 +0530

    fix: added cors

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const venderRoutes=require("./routes/vendorRoutes");
 const bodyParser=require("body-parser");
 const firmRoutes=require("./routes/firmRoutes");
 const productRoutes=require("./routes/productRoutes");
-const path= require('path');
+const cors=require("cors");
+
 
 const app= express()//inserting express into app//
-const PORT=process.env.PORT || 4000;
+
+
 
 dotenv.config();//access values in .env//
-
+app.use(cors)
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> 
     console.log("Mongodb connected successfully!"))
@@ -23,11 +25,12 @@ app.use("/firm",firmRoutes);
 app.use("/product",productRoutes);
 app.use("/uploads",express.static("uploads"));
 //start the PORT//
-app.listen(PORT,()=>{
-    console.log(`server started and running at ${PORT}`);//it gives o/p//
-})
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server started and running at ${PORT}`);
+});
 
 //route//
 app.use('/',(req,res)=>{
-    res.send("<h1> welcome t0 FOODY")
+    res.send("<h1> WELCOME TO FOODY")
 })
